9f091687957859545f7aff0df0bbeec318a5cde0
Juhan Jufri <15017395@myrp.edu.sh>
2017-05-26 10:46:31 +0800

    Initial commit

---
+++ app/src/main/java/com/myapplicationdev/android/demoalarmmanager/AlarmReceiverActivity.java
@@ -0,0 +1,13 @@
+package com.myapplicationdev.android.demoalarmmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AlarmReceiverActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_receiver);
+    }
+}
